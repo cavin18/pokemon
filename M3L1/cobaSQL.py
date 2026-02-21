53a3ae68cd037584951befbfc87fd4e6421eb61b
cavin18 <cavin.2ankara@gmail.com>
2026-02-21 17:32:51 +0700

    Initial commit dengan perbaikan tupel

---
+++ M3L1/cobaSQL.py
@@ -0,0 +1,4 @@
+import sqlite3
+
+con = sqlite3.connect('movie.db')
+con = sqlite3.connect('video.db')
--- m1L1/coba.py
+++
@@ -1,3 +0,0 @@
-for permen in range(2, 100):
-   if permen % 2 == 1 and permen % 3 != 0:
-      print(f"Jumlah permen bisa saja = {permen}")
